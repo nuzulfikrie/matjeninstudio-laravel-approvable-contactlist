47e82b469754446481970adecc8138691e466aec
nuzulfikrie <nuzulfikrie@gmail.com>
2025-10-31 07:27:10 +0800

    feat: create notification system for approval workflow
    - ApprovalRequestedNotification - notifies approvers when approval is requested
    - ApprovalApprovedNotification - notifies users when approval is approved
    - ApprovalRejectedNotification - notifies users when approval is rejected
    - All notifications support multiple channels (mail, database) via config
    - Notifications include proper mail and array representations
    - Configurable notification channels per config settings

---
+++ src/Notifications/ApprovalApprovedNotification.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MatJeninStudio\ContactApprovable\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use MatJeninStudio\ContactApprovable\Models\ApprovalRecord;
+
+class ApprovalApprovedNotification extends Notification
+{
+    use Queueable;
+
+    public function __construct(
+        public ApprovalRecord $approvalRecord
+    ) {
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     */
+    public function via($notifiable): array
+    {
+        return config('contact-approvable.notifications.channels', ['mail', 'database']);
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail($notifiable): MailMessage
+    {
+        $approval = $this->approvalRecord->approval;
+        $approvable = $approval->approvable;
+        $user = $this->approvalRecord->user;
+
+        return (new MailMessage)
+            ->subject('Approval Approved: '.class_basename($approvable))
+            ->line('Your approval request has been approved.')
+            ->line('Approved by: '.$user->name)
+            ->when($this->approvalRecord->comment, function ($mail) {
+                return $mail->line('Comment: '.$this->approvalRecord->comment);
+            })
+            ->action('View Details', url('/contact-approvable/approvals/'.$approval->id))
+            ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     */
+    public function toArray($notifiable): array
+    {
+        $approval = $this->approvalRecord->approval;
+        $approvable = $approval->approvable;
+        $user = $this->approvalRecord->user;
+
+        return [
+            'approval_id' => $approval->id,
+            'approval_record_id' => $this->approvalRecord->id,
+            'approvable_type' => $approval->approvable_type,
+            'approvable_id' => $approval->approvable_id,
+            'approved_by' => $user->name,
+            'comment' => $this->approvalRecord->comment,
+            'message' => 'Your approval request for '.class_basename($approvable).' has been approved.',
+            'url' => url('/contact-approvable/approvals/'.$approval->id),
+        ];
+    }
+}
+
---
+++ src/Notifications/ApprovalRequestedNotification.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MatJeninStudio\ContactApprovable\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use MatJeninStudio\ContactApprovable\Models\Approval;
+
+class ApprovalRequestedNotification extends Notification
+{
+    use Queueable;
+
+    public function __construct(
+        public Approval $approval
+    ) {
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     */
+    public function via($notifiable): array
+    {
+        return config('contact-approvable.notifications.channels', ['mail', 'database']);
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail($notifiable): MailMessage
+    {
+        $approvable = $this->approval->approvable;
+        $contact = $this->approval->contact;
+
+        return (new MailMessage)
+            ->subject('Approval Requested: '.class_basename($approvable))
+            ->line('A new approval has been requested for: '.class_basename($approvable))
+            ->line('Contact: '.$contact->name)
+            ->action('View Approval', url('/contact-approvable/approvals/'.$this->approval->id))
+            ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     */
+    public function toArray($notifiable): array
+    {
+        $approvable = $this->approval->approvable;
+        $contact = $this->approval->contact;
+
+        return [
+            'approval_id' => $this->approval->id,
+            'approvable_type' => $this->approval->approvable_type,
+            'approvable_id' => $this->approval->approvable_id,
+            'contact_name' => $contact->name,
+            'message' => 'A new approval has been requested for '.class_basename($approvable),
+            'url' => url('/contact-approvable/approvals/'.$this->approval->id),
+        ];
+    }
+}
+
